7f77c4b0c35728380a3db2ee8ba316daa4a8cfe9
lisazumana03 <zumanalisa@gmail.com>
2025-08-18 08:26:19 +0200

    loading

--- src/main/java/za/co/capetown/lggames/service/reservation/IBookingService.java
+++ src/main/java/za/co/capetown/lggames/service/reservation/IBookingService.java
@@ -4,5 +4,6 @@ import za.co.capetown.lggames.domain.reservation.Booking;
 import za.co.capetown.lggames.service.IService;
 
 public interface IBookingService extends IService<Booking, String> {
+    void delete(String bookingId);
     void cancelBooking(String bookingId);
 }
